454f9ba3b1736b821ee62d653af36d395a0cf20e
thmbele <th.mbele@gmail.com>
2020-08-02 03:45:16 +0500

    fix bug with wrong notations in some cases

--- math_parser/math_parser.py
+++ math_parser/math_parser.py
@@ -17,8 +17,8 @@ def parse_expression(expression: str) -> list:
 def priority(operator):
     first_operations = {'*', '/'}
     second_operations = {'+', '-'}
-    return 0 if operator in first_operations else \
-        1 if operator in second_operations else -1
+    return 1 if operator in first_operations else \
+        0 if operator in second_operations else -1
 
 
 def polish_notation(expression):
@@ -30,13 +30,27 @@ def polish_notation(expression):
         else:
             if priority(element) == -1:
                 assert ValueError
-            elif len(operations) and priority(element) >= priority(operations[-1]):
+            elif len(operations) and priority(element) <= priority(operations[-1]):
                 output.append(operations.pop())
                 operations.append(element)
             else:
                 operations.append(element)
-    output.extend(operations)
+    output.extend(operations[::-1])
     return output
 
-
-print(polish_notation("234 + 4  * 4"))
+#
+# def solve_expression(expression):
+#     expression = polish_notation(expression)
+#     print(expression)
+#     output = []
+#     for element in expression:
+#         print(output)
+#         if element[0].isdigit():
+#             output.append(int(element))
+#         else:
+#             output.append(operators.operators[element](output.pop(), output.pop()))
+#     return output
+#
+exp = "2 + 3  * 4"
+print(exp)
+print(polish_notation(exp))
