18ac4596471b8d6119130a149ca339517f4293dd
thmbele <th.mbele@gmail.com>
2020-08-02 01:08:23 +0500

    add a expression to list parser

--- math_parser.py
+++ math_parser.py
@@ -1,5 +1,24 @@
-def parse(expression):
-    expression = expression.replace(' ', '')
+def parse_expression(expression):
+    expression = expression.replace(' ', '') + '\\'  # backslash is added to know the end of string
+    tmp_expression = []
+    tmp_ch = ''
+    for ch in expression:
+        if ch.isdigit():
+            tmp_ch += ch
+        else:
+            tmp_expression.append(tmp_ch)
+            tmp_expression.append(ch)
+            tmp_ch = ''
+    return tmp_expression[:-1]
 
 
-parse("2 +   2")
+def solve_expression(expression):
+    expression = parse_expression(expression)
+    first_operations = {'*', '/', '(', ')'}
+    second_operations = {'+', '-'}
+    output, operations = [], []
+    print(expression)
+    return
+
+
+solve_expression("234 +   2")
