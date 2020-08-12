b78334dcebcd57ecbfd26d051c43cf6ea4ac6600
thmbele <th.mbele@gmail.com>
2020-08-13 03:48:05 +0500

    Add brackets support

--- math_parser/math_parser.py
+++ math_parser/math_parser.py
@@ -1,4 +1,4 @@
-from math_parser.operators import operators
+from operators import *
 
 
 def parse_expression(expression: str) -> list:
@@ -16,8 +16,8 @@ def parse_expression(expression: str) -> list:
 
 
 def priority(operator):
-    first_operations = {'*', '/'}
-    second_operations = {'+', '-'}
+    first_operations = ('*', '/')
+    second_operations = ('+', '-')
     return 1 if operator in first_operations else \
         0 if operator in second_operations else -1
 
@@ -26,16 +26,21 @@ def polish_notation(expression):
     expression = parse_expression(expression)
     output, operations = [], []
     for element in expression:
-        if element[0].isdigit():
+        if element.isdigit():
             output.append(element)
+        elif element == '(':
+            operations.append(element)
+        elif element == ')':
+            bracket_index = len(operations) - operations[::-1].index('(') - 1
+            output.extend(operations[bracket_index + 1:])
+            del operations[bracket_index:]
+        elif priority(element) == -1:
+            assert ValueError
+        elif len(operations) and priority(element) <= priority(operations[-1]):
+            output.append(operations.pop())
+            operations.append(element)
         else:
-            if priority(element) == -1:
-                assert ValueError
-            elif len(operations) and priority(element) <= priority(operations[-1]):
-                output.append(operations.pop())
-                operations.append(element)
-            else:
-                operations.append(element)
+            operations.append(element)
     output.extend(operations[::-1])
     return output
 
@@ -49,3 +54,6 @@ def solve_expression(expression):
         else:
             output.append(operators[element](output.pop(-2), output.pop()))
     return output.pop()
+
+
+print(solve_expression('(6+4) * (2 + (1 + 2 + 1))'))
