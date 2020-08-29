9a0f15a0664fcb8239df080a5593aa7c3dc24c44
thmbele <th.mbele@gmail.com>
2020-08-30 02:01:41 +0500

    Add split in commands to call different functions

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,8 @@
 from telebot import types
 import settings
 from math_parser.math_parser import solve_expression
+from layout_switcher.layout_switcher import translate_to_cyrillic
+from layout_switcher.layout_switcher import translate_to_latin
 import telebot
 
 
@@ -17,6 +19,18 @@ couffaine = telebot.TeleBot(settings.TELEBOT_TOKEN)
 # awake()
 
 
+def solve_query(query):
+    answer = str(solve_expression(query.query))
+    result = types.InlineQueryResultArticle(
+        id='1',
+        title=query.query + ' = ' + answer,
+        description='Solved!',
+        input_message_content=
+        types.InputTextMessageContent(message_text=query.query + ' = ' + answer)
+    )
+    couffaine.answer_inline_query(query.id, [result])
+
+
 @couffaine.message_handler(commands=['start'])
 def start_handler(message):
     greeting_message = "Hey, {0}! I'm Couffaine. What can i do for you?".format(str(message.from_user.first_name))
@@ -26,18 +40,16 @@ def start_handler(message):
 
 @couffaine.inline_handler(func=lambda query: len(query.query) > 0)
 def query_text(query):
-    try:
-        answer = str(solve_expression(query.query))
-        result = types.InlineQueryResultArticle(
-            id='1',
-            title=query.query + ' = ' + answer,
-            description='Solved!',
-            input_message_content=
-            types.InputTextMessageContent(message_text=query.query + ' = ' + answer)
-        )
-        couffaine.answer_inline_query(query.id, [result])
-    except (IndexError, KeyError) as exc:
-        print(exc)
+    command = query.query.split()[0]
+    if command[0] == 's':  # stands for solve
+        try:
+            solve_query(query)
+        except (IndexError, KeyError) as exc:
+            print("Failed to solve:", exc)
+    elif command[0] == 't':  # stands for translate
+        split_text = query.query.split()
+        layout, text = split_text[1], ' '.join(split_text[2:])
+        print(layout, text)
 
 
 couffaine.polling()
