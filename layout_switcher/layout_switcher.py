c3831dddc2ab3704831107f6a0b3ac1cb2391f88
thmbele <th.mbele@gmail.com>
2020-08-30 00:16:40 +0500

    Refactor code

--- layout_switcher/layout_switcher.py
+++ layout_switcher/layout_switcher.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 
-cyrillic_layout = "йцукенгшщзхъ" + \
-                  "фывапролджэ" + \
-                  "ячсмитьбю." + \
-                  "!\"№;%:?*()_+" + \
-                  "ЙЦУКЕНГШЩЗХЪ" + \
-                  "ФЫВАПРОЛДЖЭ" + \
-                  "ЯЧСМИТЬБЮ, " + \
-                  "1234567890-="
+cyrillic_layout = """
+            йцукенгшщзхъ\
+            фывапролджэ
+            ячсмитьбю.
+            ЙЦУКЕНГШЩЗХЪ/
+            ФЫВАПРОЛДЖЭ
+            ЯЧСМИТЬБЮ,
+            Ё!"№;%:?*()_+
+            ё1234567890-=
+            """
 
-latin_layout = "qwertyuiop[]" + \
-               "asdfghjkl;'" + \
-               "zxcvbnm,./" + \
-               "!@#$%^&*()_+" + \
-               "QWERTYUIOP{}" + \
-               "ASDFGHJKL:\"" + \
-               "ZXCVBNM<>? " + \
-               "1234567890-="
+latin_layout = """
+            qwertyuiop[]\
+            asdfghjkl;'
+            zxcvbnm,./
+            QWERTYUIOP{}|
+            ASDFGHJKL:"
+            ZXCVBNM<>?
+            ~!@#$%^&*()_+
+            `1234567890-=
+            """
 
 cyrillic_to_latin = dict(zip(cyrillic_layout, latin_layout))
 latin_to_cyrillic = dict(zip(latin_layout, cyrillic_layout))
